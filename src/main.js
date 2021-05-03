d6e1723d8986ad893555d4db28abb75991b5dd39
Yennys <yenny1989@gmail.com>
2021-05-03 12:36:31 -0500

    Estructura SPA

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 // Este es el punto de entrada de tu aplicacion
 
-import { myFunction } from './lib/index.js';
-
-myFunction();
+import changeViews from './routeController/routing.js';
+const init = () => {
+    changeViews(window.location.hash);
+    window.addEventListener('hashchange' , () => changeViews(window.location.hash));
+}
+window.addEventListener('load', init); //Cada vez que recargue ejecuta esta ()
