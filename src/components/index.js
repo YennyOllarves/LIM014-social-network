696bbf91f5a8c8839ba4f5abc7cbd2c5556a842d
wendyhachilla <wendyhachilla2@gmail.com>
2021-05-03 14:28:41 -0500

    advance

---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+import viewHeader from '../views/header';
+import viewRegister from '../views/register.js';
+
+const components = {
+  Header: viewHeader,
+  Register: viewRegister,
+};
+
+export default components;
