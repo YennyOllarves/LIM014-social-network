9e76b1ffd7b7717c1185787dbdcd908117a948d5
wendyhachilla <wendyhachilla2@gmail.com>
2021-05-10 11:10:34 -0500

    auth

--- src/firebase-controllers/auth-controller.js
+++ src/firebase-controllers/auth-controller.js
@@ -1,35 +1,34 @@
 // Login UserPass
 const loginEmail = (email, password) => {
-    const auth = firebase.auth();
-    return auth.signInWithEmailAndPassword(email, password);
+  const auth = firebase.auth();
+  return auth.signInWithEmailAndPassword(email, password);
 };
 
 // Login Google
 const loginGoogle = () => {
-    const auth = firebase.auth();
-    const googleProvider = new firebase.auth.GoogleAuthProvider();
-    return auth.signInWithRedirect(googleProvider);
-    // return auth.signInWithPopup(googleProvider); 
+  const auth = firebase.auth();
+  const googleProvider = new firebase.auth.GoogleAuthProvider();
+  return auth.signInWithRedirect(googleProvider);
+  // return auth.signInWithPopup(googleProvider);
 };
 
 // Creación de Usuarios
 const userRegister = (email, password) => {
-const auth = firebase.auth();
-return auth.createUserWithEmailAndPassword(email, password);
+  const auth = firebase.auth();
+  return auth.createUserWithEmailAndPassword(email, password);
 };
 
 // Verificar Mail
 const checkMail = () => {
-    const user = firebase.auth().currentUser();
-    return user.sendEmailVerification();
-
+  const user = firebase.auth().currentUser();
+  return user.sendEmailVerification();
 };
 
+// usuario actual
 const currentUser = firebase.auth().currentUser();
 
 // Cerrar Sesión
 const logOut = () => firebase.auth().singOut();
-
 
 // Recuperar Pass
 /* const recoverPassword = (email) => {
@@ -37,15 +36,12 @@ const logOut = () => firebase.auth().singOut();
     return auth.sendPasswordResetEmail(email);
 } */
 
-
-
 export {
-    loginEmail,
-    loginGoogle,
-    userRegister,
-    checkMail,
-    logOut
+  loginEmail,
+  loginGoogle,
+  userRegister,
+  checkMail,
+  logOut,
+  currentUser,
 
-
-
-}  
+};
