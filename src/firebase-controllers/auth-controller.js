25fcf0e107b17179ce029198923767784a44bd46
PaulaFernandezTeves <paulafernandezteves@gmail>
2021-05-13 21:53:50 -0500

    update

--- src/firebase-controllers/auth-controller.js
+++ src/firebase-controllers/auth-controller.js
@@ -1,11 +1,11 @@
 // Login UserPass
-const loginEmail = (email, password) => {
+export const loginEmail = (email, password) => {
   const auth = firebase.auth();
   return auth.signInWithEmailAndPassword(email, password);
 };
 
 // Login Google
-const loginGoogle = () => {
+export const loginGoogle = () => {
   const auth = firebase.auth();
   const googleProvider = new firebase.auth.GoogleAuthProvider();
   return auth.signInWithRedirect(googleProvider);
@@ -13,22 +13,22 @@ const loginGoogle = () => {
 };
 
 // Creación de Usuarios
-const userRegister = (email, password) => { // consulta de agregar name al parametro
+export const userRegister = (email, password) => { // consulta de agregar name al parametro
   const auth = firebase.auth();
   return auth.createUserWithEmailAndPassword(email, password);
 };
 
 // Verificar Mail
-const checkMail = () => {
+export const checkMail = () => {
   const user = firebase.auth().currentUser;
   return user.sendEmailVerification();
 };
 
 // usuario actual
-const currentUser = () => firebase.auth().currentUser;
+export const currentUser = () => firebase.auth().currentUser;
 
 // Cerrar Sesión
-const logOut = () => firebase.auth().singOut();
+export const logOut = () => firebase.auth().logOut;
 
 // Recuperar Pass
 /* const recoverPassword = (email) => {
@@ -36,11 +36,11 @@ const logOut = () => firebase.auth().singOut();
     return auth.sendPasswordResetEmail(email);
 } */
 
-export {
-  loginEmail,
-  loginGoogle,
-  userRegister,
-  checkMail,
-  logOut,
-  currentUser,
-};
+// export {
+//   loginEmail,
+//   loginGoogle,
+//   userRegister,
+//   checkMail,
+//   logOut,
+//   currentUser,
+// };
--- src/views/menu.js
+++ src/views/menu.js
@@ -1,18 +1,2 @@
 // PLANTILLA ELEMENTOS DEL MENU DESPLEGABLE (HEADER)
 
-export default () => {
-  const viewMenu = document.createElement('section');
-  viewMenu.classList.add('main-header');
-  viewMenu.innerHTML = `
-    <nav>
-      <ul class="menu-header">
-        <div id="left-menu-header" class= "box">
-          <li class="home-header"><a href="#/inicio"><span class="fas fa-home sitio-web--icon"> Inicio </span></a></li>
-          <li class="profile-header"><a href="#/profile"><span class="fas fa-user-circle"> Profile </span></a></li>
-        </div>
-        <li id="log-out-header" class="box"><span class="fas fa-power-off"> Cerrar sesión </span></li>
-      </ul>
-    </nav>
-    <i id="hamburger-menu" class="fas fa-bars hide"></i>
-    `;
-};
