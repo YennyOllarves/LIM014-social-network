f92bf24a2687f9e24189dc890771ea686909c09a
wendyhachilla <wendyhachilla2@gmail.com>
2021-05-15 19:09:00 -0500

    index

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,13 @@
 import viewLogin from '../views/loginPage.js';
 import viewRegister from '../views/registrationPage.js';
 import viewMenu from '../views/header.js';
+import viewHomePage from '../views/home.js';
 
 const components = {
   login: viewLogin,
   register: viewRegister,
   menuHeader: viewMenu,
+  home: viewHomePage,
 };
 
 export default components;
