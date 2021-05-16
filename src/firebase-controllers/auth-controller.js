1fe0618abfe9d54fd194401e86ab76e1760096cc
wendyhachilla <wendyhachilla2@gmail.com>
2021-05-15 19:09:24 -0500

    auth

--- src/firebase-controllers/auth-controller.js
+++ src/firebase-controllers/auth-controller.js
@@ -1,11 +1,11 @@
 // Login UserPass
-export const loginEmail = (email, password) => {
+const loginEmail = (email, password) => {
   const auth = firebase.auth();
   return auth.signInWithEmailAndPassword(email, password);
 };
 
 // Login Google
-export const loginGoogle = () => {
+const loginGoogle = () => {
   const auth = firebase.auth();
   const googleProvider = new firebase.auth.GoogleAuthProvider();
   return auth.signInWithRedirect(googleProvider);
@@ -13,22 +13,22 @@ export const loginGoogle = () => {
 };
 
 // Creación de Usuarios
-export const userRegister = (email, password) => { // consulta de agregar name al parametro
+const userRegister = (email, password) => { // consulta de agregar name al parametro
   const auth = firebase.auth();
   return auth.createUserWithEmailAndPassword(email, password);
 };
 
 // Verificar Mail
-export const checkMail = () => {
+const checkMail = () => {
   const user = firebase.auth().currentUser;
   return user.sendEmailVerification();
 };
 
 // usuario actual
-export const currentUser = () => firebase.auth().currentUser;
+const currentUser = () => firebase.auth().currentUser;
 
 // Cerrar Sesión
-export const logOut = () => firebase.auth().logOut;
+const logOut = () => firebase.auth().logOut;
 
 // Recuperar Pass
 /* const recoverPassword = (email) => {
@@ -36,11 +36,11 @@ export const logOut = () => firebase.auth().logOut;
     return auth.sendPasswordResetEmail(email);
 } */
 
-// export {
-//   loginEmail,
-//   loginGoogle,
-//   userRegister,
-//   checkMail,
-//   logOut,
-//   currentUser,
-// };
+export {
+  loginEmail,
+  loginGoogle,
+  userRegister,
+  checkMail,
+  logOut,
+  currentUser,
+};
