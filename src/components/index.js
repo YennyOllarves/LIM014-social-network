c31f390ddeb95d8c0e125f82446c76cc3ac57c38
PaulaFernandezTeves <paulafernandezteves@gmail>
2021-05-17 09:14:56 -0500

    index.js

--- src/components/index.js
+++ src/components/index.js
@@ -2,13 +2,14 @@ import viewLogin from '../views/loginPage.js';
 import viewRegister from '../views/registrationPage.js';
 import viewMenu from '../views/header.js';
 import viewHomePage from '../views/home.js';
-import 
+import viewPost from '../views/post.js';
 
 const components = {
   login: viewLogin,
   register: viewRegister,
   menuHeader: viewMenu,
   home: viewHomePage,
+  post: viewPost,
 };
 
 export default components;
