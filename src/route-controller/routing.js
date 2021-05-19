7d3a940c7f570ba5a922b74f03f5f431344f0e51
wendyhachilla <wendyhachilla2@gmail.com>
2021-05-19 00:46:13 -0500

    update5

--- src/route-controller/routing.js
+++ src/route-controller/routing.js
@@ -1,5 +1,6 @@
 // eslint-disable-next-line import/named
 import components from '../components/index.js';
+
 // import {getUserData} from '../firebase-controllers/fireStore-controller.js';
 // import {currentUser} from '../firebase-controllers/auth-controller.js';
 
@@ -10,6 +11,9 @@ const changeViews = (route) => {
   container.innerHTML = '';
   header.innerHTML = '';
   switch (route) {
+    // case '':
+    //   container.appendChild(components.login());  
+    //   break;
     case '#/login':
       container.appendChild(components.login());
       break;
